638a5eb77be737f247d0398761bf46986d7017b9
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-03-09 16:48:53 -0300

    Initial project
    Initial project

---
+++ src/test/java/com/awssqsspringboot/AwsSqsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awssqsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
