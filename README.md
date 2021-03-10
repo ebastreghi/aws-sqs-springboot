b71e1e93f79fa66d7dd356418b38f4a5be19a781
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-03-10 16:05:11 -0300

    PostMessageJMS
    PostMessageJMS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-
+AWS SQS with AWS lib and JMS(Producer and Consumer)
 
---
+++ src/main/java/com/awssqsspringboot/sqs/PostMessageJMS.java
@@ -0,0 +1,44 @@
+package com.awssqsspringboot.sqs;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnection;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.jms.*;
+
+
+public class PostMessageJMS {
+
+    @Value("${aws.access-key}")
+    private static String accessKey;
+
+    @Value("${aws.secret-key}")
+    private static String secretKey;
+
+    @Value("${aws.sqs-resource}")
+    private static String sqsResource;
+
+    public static void main(String args[]) throws JMSException {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        AmazonSQS sqsClient = AmazonSQSClient.builder().withRegion(Regions.US_EAST_2)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+
+        SQSConnectionFactory connectionFactory = new SQSConnectionFactory(new ProviderConfiguration(), sqsClient);
+        SQSConnection connection = connectionFactory.createConnection();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue("aws-sqs-springboot");
+        MessageProducer producer = session.createProducer(queue);
+
+        TextMessage message = session.createTextMessage("test sending a message run 1");
+        producer.send(message);
+    }
+}
